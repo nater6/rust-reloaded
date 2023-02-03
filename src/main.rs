7dbe2ce807bfca0fb103eff5276d9de8d11afb28
nater68 <n.d.russell@hotmail.com>
2023-02-03 13:42:40 +0000

    Attempting to read the file contents

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::fs;
 fn main() {
     //Scan through text file and print out each line
-    
+    let file_path = "../resources/sample.txt";
+    let _content = fs::read_to_string(file_path).expect("Error reading the file contents");
+    println!("Content: \n");
 }
